c04344ea59a7a5f093b97f894b640dd02ad9ab9a
guominglong <guominglong@51talk.com>
2020-04-26 16:01:27 +0800

    新增sudo权限

--- restart.sh
+++ restart.sh
@@ -1,16 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
 while true
 
 do
 
-	ps -ef | grep "appMain" | grep -v "grep"
+	sudo ps -ef | grep "appMain" | grep -v "grep"
 
 	if [ "$?" -eq 1 ]
 
 	then
 
-		./run.sh #启动应用，修改成自己的启动应用脚本或命令
+		sudo ./run.sh #启动应用，修改成自己的启动应用脚本或命令
 
 		echo "appMain has been restarted!"
 
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 sudo rm -rf ./nohup.out
 sudo nohup ./appMain &
