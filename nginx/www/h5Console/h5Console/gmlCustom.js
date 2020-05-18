b986c939bbfdd4fc8b958cccdcb2d8da8bef07f1
guominglong <guominglong@51talk.com>
2020-05-18 20:01:14 +0800

    fix:字符替换问题

--- nginx/www/h5Console/h5Console/gmlCustom.js
+++ nginx/www/h5Console/h5Console/gmlCustom.js
@@ -80,7 +80,7 @@ function onStartTestingClick(data){
         let endTimeStr = data.field["tb_testing_end_time"];
         let offline_timeoffset = startTestArgsObj["offline_timeoffset"];
         if(endTimeStr && endTimeStr != ""){
-            endTimeStr = endTimeStr.replace("-","/");
+            endTimeStr = endTimeStr.replace(/[-]/ig,"/");
             //定时停止
             let endTime = new Date(endTimeStr).valueOf();
             let nowTime = new Date().valueOf();
